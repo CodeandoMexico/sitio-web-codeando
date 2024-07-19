19fe5ff1b439abd4ee8adf63e83ea4a18099d0f4
Hans Ramírez <hans@thehackermonkey.com>
2024-07-19 17:54:47 -0600

    fix(unused): hide not developed sections for now

--- src/lib/apiClient.js
+++ src/lib/apiClient.js
@@ -1,7 +1,5 @@
 import { createDirectus, rest, staticToken } from '@directus/sdk'
 import { DIRECTUS_TOKEN } from '$env/static/private'
-console.log("el token")
-console.log(DIRECTUS_TOKEN)
 
 const client = createDirectus('https://content.codeandomexico.org').with(rest()).with(staticToken(DIRECTUS_TOKEN))
 
