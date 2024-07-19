126ef363da6ea3170af7462f95c159422fe924af
Hans Ramírez <hans@thehackermonkey.com>
2024-07-19 14:03:55 -0600

    fix(debug): added statment for debugging

--- src/lib/apiClient.js
+++ src/lib/apiClient.js
@@ -1,5 +1,7 @@
 import { createDirectus, rest, staticToken } from '@directus/sdk'
 import { DIRECTUS_TOKEN } from '$env/static/private'
+console.log("el token")
+console.log(DIRECTUS_TOKEN)
 
 const client = createDirectus('https://content.codeandomexico.org').with(rest()).with(staticToken(DIRECTUS_TOKEN))
 
